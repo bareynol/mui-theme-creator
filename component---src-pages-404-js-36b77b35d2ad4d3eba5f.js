acae8af80d1b0a1ee07774083e2293aa44ce2561
Brian Reynolds <brireyn@gmail.com>
2020-08-25 14:17:11 -0400

    Updates

---
+++ component---src-pages-404-js-36b77b35d2ad4d3eba5f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("soUV"),o=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-36b77b35d2ad4d3eba5f.js.map
