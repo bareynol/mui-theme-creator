47f3028227ec9118f7ab560203b3ffd5f5f7aa90
Darren Rush <dlrush@gmail.com>
2023-08-26 13:45:47 -0700

    Add variant='filled' examples

--- src/components/MuiComponentSamples/Samples/Alert.tsx
+++ src/components/MuiComponentSamples/Samples/Alert.tsx
@@ -1,9 +1,18 @@
 import { Alert, AlertTitle, Grid } from "@mui/material"
 import React from "react"
 
+const sectionProps = {
+  item: true,
+  xs: 12,
+  lg: 6,
+  xl: 4,
+  gap: 2,
+  sx: { display: "flex", flexDirection: "column" },
+}
+
 const AlertExample = () => (
-  <Grid container gap={2}>
-    <Grid gap={2} sx={{display: 'flex', flexDirection: 'column'}}>
+  <Grid container spacing={2}>
+    <Grid {...sectionProps}>
       <Alert severity="error">
         <AlertTitle>Error</AlertTitle>
         This is an error alert — <strong>check it out!</strong>
@@ -22,7 +31,7 @@ const AlertExample = () => (
       </Alert>
     </Grid>
 
-    <Grid gap={2} sx={{display: 'flex', flexDirection: 'column'}}>
+    <Grid {...sectionProps}>
       <Alert severity="error" variant="outlined">
         <AlertTitle>Error</AlertTitle>
         This is an error alert — <strong>check it out!</strong>
@@ -36,6 +45,25 @@ const AlertExample = () => (
         This is an info alert — <strong>check it out!</strong>
       </Alert>
       <Alert severity="success" variant="outlined">
+        <AlertTitle>Success</AlertTitle>
+        This is a success alert — <strong>check it out!</strong>
+      </Alert>
+    </Grid>
+
+    <Grid {...sectionProps}>
+      <Alert severity="error" variant="filled">
+        <AlertTitle>Error</AlertTitle>
+        This is an error alert — <strong>check it out!</strong>
+      </Alert>
+      <Alert severity="warning" variant="filled">
+        <AlertTitle>Warning</AlertTitle>
+        This is a warning alert — <strong>check it out!</strong>
+      </Alert>
+      <Alert severity="info" variant="filled">
+        <AlertTitle>Info</AlertTitle>
+        This is an info alert — <strong>check it out!</strong>
+      </Alert>
+      <Alert severity="success" variant="filled">
         <AlertTitle>Success</AlertTitle>
         This is a success alert — <strong>check it out!</strong>
       </Alert>
